5ee935e3a423b2df5a4e9441832f75e53aaafe8c
laureanray <laureanraybahala@gmail.com>
2023-07-23 16:59:27 +0800

    fix test case

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,8 +15,9 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -23,7 +25,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -50,7 +58,7 @@ pub fn search_case_insensitive<'a>(
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.contains(&query) {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -82,7 +90,7 @@ Pick three.
 Trust me.";
 
         assert_eq!(
-            vec!["Rust:", "Trust Me."],
+            vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
     }
