72edfb84554e867ce5094e0897915b461bfbb233
laureanray <laureanraybahala@gmail.com>
2023-07-23 15:27:43 +0800

    remove println stmts

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
-
     Ok(())
 }
 
